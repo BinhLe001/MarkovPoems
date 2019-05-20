b4de8de3fb78d628b6b9b9e8e76cc1e3b7490c41
Binh Le <binhle@mit.edu>
2019-05-20 01:31:32 -0400

    Updates

---
+++ precache-manifest.2d7b339c5faa6c6e60b6fc62958a8fb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdead6d99dc926a493eaf422363304f3",
+    "url": "/MarkovPoems/index.html"
+  },
+  {
+    "revision": "a068f05ba284abcb6d1b",
+    "url": "/MarkovPoems/static/css/main.2556fd39.chunk.css"
+  },
+  {
+    "revision": "ef60339c4d2aed56294d",
+    "url": "/MarkovPoems/static/js/2.52a6492a.chunk.js"
+  },
+  {
+    "revision": "a068f05ba284abcb6d1b",
+    "url": "/MarkovPoems/static/js/main.8c9ab248.chunk.js"
+  },
+  {
+    "revision": "90327b7b0304b29a15b7",
+    "url": "/MarkovPoems/static/js/runtime~main.dac9a88b.js"
+  }
+]);
